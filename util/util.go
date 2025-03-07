9adcb581237e33423d53ff963d1b69ee41f96dbc
MmadF14 <mmadf14@gmail.com>
2025-03-08 02:13:37 +0330

    007

--- util/util.go
+++ util/util.go
@@ -390,14 +390,8 @@ func ValidateIPAllocation(serverAddresses []string, ipAllocatedList []string, ip
 
 	// تبدیل IP های تخصیص داده شده به map برای جستجوی سریع‌تر
 	allocatedMap := make(map[string]bool)
-	for _, allocatedCIDR := range ipAllocatedList {
-		ip, _, err := net.ParseCIDR(allocatedCIDR)
-		if err != nil {
-			// اگر در فرمت CIDR نیست، مستقیم از خودش استفاده کن
-			allocatedMap[allocatedCIDR] = true
-		} else {
-			allocatedMap[ip.String()] = true
-		}
+	for _, ip := range ipAllocatedList {
+		allocatedMap[ip] = true
 	}
 
 	for _, clientCIDR := range ipAllocationList {
@@ -420,10 +414,10 @@ func ValidateIPAllocation(serverAddresses []string, ipAllocatedList []string, ip
 			return false, fmt.Errorf("IP %s does not belong to any network addresses of WireGuard server", ip)
 		}
 
-		// بررسی تکراری نبودن IP - فقط مقایسه IP بدون CIDR
-		if allocatedMap[ip.String()] {
-			return false, fmt.Errorf("IP %s already allocated", ip)
-		}
+		// بررسی تکراری نبودن IP
+		// if allocatedMap[ip.String()] {
+		// 	return false, fmt.Errorf("IP %s already allocated", ip)
+		// }
 	}
 
 	return true, nil
