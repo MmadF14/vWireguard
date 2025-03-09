484a109130b095256ee3b586d8cd78c1ef44b915
MmadF14 <mmadf14@gmail.com>
2025-03-09 15:04:47 +0330

    Update prepare_assets.sh

--- prepare_assets.sh
+++ prepare_assets.sh
@@ -8,17 +8,23 @@ YARN=yarn
 [ -x /usr/bin/lsb_release ] && [ -n "`lsb_release -i | grep Debian`" ] && YARN=yarnpkg
 $YARN install --pure-lockfile --production
 
+# Create necessary directories
+mkdir -p "${DIR}/assets/dist/js" "${DIR}/assets/dist/css"
+
 # Copy admin-lte dist
-mkdir -p "${DIR}/assets/dist/js" "${DIR}/assets/dist/css" && \
-  cp -r "${DIR}/node_modules/admin-lte/dist/js/adminlte.min.js" "${DIR}/assets/dist/js/adminlte.min.js" && \
-  cp -r "${DIR}/node_modules/admin-lte/dist/css/adminlte.min.css" "${DIR}/assets/dist/css/adminlte.min.css"
+cp -r "${DIR}/node_modules/admin-lte/dist/js/adminlte.min.js" "${DIR}/assets/dist/js/adminlte.min.js"
+cp -r "${DIR}/node_modules/admin-lte/dist/css/adminlte.min.css" "${DIR}/assets/dist/css/adminlte.min.css"
+
+# Copy custom files
+cp -r "${DIR}/static/dist/css/custom.css" "${DIR}/assets/dist/css/custom.css"
+cp -r "${DIR}/static/dist/js/custom.js" "${DIR}/assets/dist/js/custom.js"
 
 # Copy helper js
 cp -r "${DIR}/custom" "${DIR}/assets"
 
 # Copy plugins
-mkdir -p "${DIR}/assets/plugins" && \
-  cp -r "${DIR}/node_modules/admin-lte/plugins/jquery" \
+mkdir -p "${DIR}/assets/plugins"
+cp -r "${DIR}/node_modules/admin-lte/plugins/jquery" \
   "${DIR}/node_modules/admin-lte/plugins/fontawesome-free" \
   "${DIR}/node_modules/admin-lte/plugins/bootstrap" \
   "${DIR}/node_modules/admin-lte/plugins/icheck-bootstrap" \
@@ -27,3 +33,11 @@ mkdir -p "${DIR}/assets/plugins" && \
   "${DIR}/node_modules/admin-lte/plugins/select2" \
   "${DIR}/node_modules/jquery-tags-input" \
   "${DIR}/assets/plugins/"
+
+# Create static directory structure
+mkdir -p "${DIR}/static/dist/js" "${DIR}/static/dist/css"
+
+# Copy assets to static directory
+cp -r "${DIR}/assets/dist/js/"* "${DIR}/static/dist/js/"
+cp -r "${DIR}/assets/dist/css/"* "${DIR}/static/dist/css/"
+cp -r "${DIR}/assets/plugins" "${DIR}/static/"
