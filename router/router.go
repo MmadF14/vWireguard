92b0b9d1877a8e2bc34df9d8f0a337152d2e9dca
MmadF14 <mmadf14@gmail.com>
2025-03-09 15:25:44 +0330

    01.03

--- router/router.go
+++ router/router.go
@@ -162,5 +162,11 @@ func New(tmplDir fs.FS, extraData map[string]interface{}, secret [64]byte) *echo
 	// Static files
 	e.Static("/static", "static")
 
+	// Add CSRF protection
+	e.Use(middleware.CSRFWithConfig(middleware.CSRFConfig{
+		TokenLookup: "form:_csrf",
+		CookiePath:  cookiePath,
+	}))
+
 	return e
 }
