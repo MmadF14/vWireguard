de0a9ab94308e58e9b6b6cdc617e5a2b35ea4f01
MmadF14 <133948087+MmadF14@users.noreply.github.com>
2025-03-16 14:47:09 +0330

    Update utilities.go

--- handler/utilities.go
+++ handler/utilities.go
@@ -3,17 +3,23 @@ package handler
 import (
 	"net/http"
 
+	"github.com/MmadF14/vwireguard/model"
+	"github.com/MmadF14/vwireguard/store"
 	"github.com/labstack/echo/v4"
 )
 
 // UtilitiesPage handles the utilities page request
-func UtilitiesPage(c echo.Context) error {
-	baseData := GetBaseData(c)
-	baseData.Active = "utilities"
+func UtilitiesPage(db store.IStore) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		user, _ := db.GetUserByName(currentUser(c))
+		data := map[string]interface{}{
+			"baseData": model.BaseData{
+				Active:      "utilities",
+				CurrentUser: currentUser(c),
+				Admin:      user.Role == model.RoleAdmin,
+			},
+		}
 
-	data := map[string]interface{}{
-		"baseData": baseData,
+		return c.Render(http.StatusOK, "utilities.html", data)
 	}
-
-	return c.Render(http.StatusOK, "utilities.html", data)
 } 
