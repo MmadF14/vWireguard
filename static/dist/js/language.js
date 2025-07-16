1a915de80043c20eb13c0f91663879d39d3bfdd9
MmadF14 <133948087+MmadF14@users.noreply.github.com>
2025-07-16 20:57:49 +0330

    remove language manager debug logs

--- static/dist/js/language.js
+++ static/dist/js/language.js
@@ -35,10 +35,8 @@ class LanguageManager {
     }
 
     translatePage() {
-        console.log('Translating page to:', this.currentLang);
         // First, translate all elements with data-translate attribute
         const elementsToTranslate = document.querySelectorAll('[data-translate]');
-        console.log('Found', elementsToTranslate.length, 'elements to translate');
         
         elementsToTranslate.forEach(element => {
             const key = element.getAttribute('data-translate');
